97d2a131869c3d3f97870e4ed79cc5fe814a58b4
tofa93 <tofig.valiyev1993@gmail.com>
2023-05-19 22:17:23 +0200

    added new features

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const resultField = document.querySelector('.results-field');
 const countBtn = document.querySelector('.count-btn');
 const randBtn = document.querySelector('.rand-btn');
 const selectMenu = document.querySelector('.select-menu');
+const donwloadBtn = document.querySelector('.download-btn');
+const colorToggler = document.querySelector('.color-toggler');
 
 
 function countNum() {
@@ -27,13 +29,36 @@ function countNum() {
     } else {
         resultField.innerHTML = "Enter a valid number and operand";
     }
-
-
-    inputField.value = '';
+    donwloadBtn.style.display = 'flex';
 }
 
 function randNum() {
     let randomNumber = Math.random() * 11;
     inputField.value = Math.floor(randomNumber);
-    // console.log('random button');
 }
+
+function downloadData() {
+    var dataToDownload = "Operation: " + selectMenu.value + "\n" +
+        "Input Value: " + inputField.value + "\n" +
+        "Result: " + resultField.value;
+    var blob = new Blob([dataToDownload], { type: "text/plain" });
+    var downloadLink = document.createElement("a");
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = "data.txt";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
+colorToggler.addEventListener('click', function () {
+    if (!colorToggler.checked) {
+        inputField.style.backgroundColor = '#cad2c5';
+        inputField.style.color = '#101010';
+        resultField.style.backgroundColor = '#cad2c5';
+        resultField.style.color = '#101010';
+    } else {
+        inputField.style.backgroundColor = '';
+        inputField.style.color = '';
+        resultField.style.backgroundColor = '';
+        resultField.style.color = '';
+    }
+})
