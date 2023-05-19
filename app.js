a0172e53becf42fa3d5aedafe8d615a5e58d1e37
tofa93 <tofig.valiyev1993@gmail.com>
2023-05-19 18:38:53 +0200

    adding new features

---
+++ app.js
@@ -0,0 +1,4 @@
+const inputField = document.querySelector('.number-field');
+const resultField = document.querySelector('.results-field');
+const countBtn = document.querySelector('.count-btn');
+const randBtn = document.querySelector('.rand-btn');
