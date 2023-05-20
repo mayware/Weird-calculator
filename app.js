48b54e3bca8f17c625186f61018758774ddfa1d8
tofa93 <tofig.valiyev1993@gmail.com>
2023-05-20 16:24:01 +0200

    Finished the main part

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const countBtn = document.querySelector('.count-btn');
 const randBtn = document.querySelector('.rand-btn');
 const selectMenu = document.querySelector('.select-menu');
 const donwloadBtn = document.querySelector('.download-btn');
+const downloadBlock = document.querySelector('.download-block');
 const colorToggler = document.querySelector('.color-toggler');
 
 
@@ -13,23 +14,23 @@ function countNum() {
     const numbersArray = inpNum.split(" ");
     const numbers = numbersArray.map(num => parseInt(num));
     if (inpNum !== '' && selectedOption !== '') {
+        downloadBlock.style.display = 'flex';
         if (selectedOption == 'sum') {
             let sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-            resultField.innerHTML = sum;
+            resultField.value = sum;
         } else if (selectedOption == 'prod') {
             let prod = numbers.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
-            resultField.innerHTML = prod;
+            resultField.value = prod;
         } else if (selectedOption == 'min') {
             let min = Math.min(...numbers);
-            resultField.innerHTML = min;
+            resultField.value = min;
         } else if (selectedOption == 'max') {
             let max = Math.max(...numbers);
-            resultField.innerHTML = max;
+            resultField.value = max;
         }
     } else {
         resultField.innerHTML = "Enter a valid number and operand";
     }
-    donwloadBtn.style.display = 'flex';
 }
 
 function randNum() {
@@ -48,7 +49,11 @@ function downloadData() {
     var blob = new Blob([dataToDownload], { type: "text/plain" });
     var downloadLink = document.createElement("a");
     downloadLink.href = URL.createObjectURL(blob);
-    downloadLink.download = "data.txt";
+    if (downloadInput.value !== '') {
+        downloadLink.download = downloadInput.value + '.txt';
+    } else {
+        downloadLink.download = 'results.txt'
+    }
     document.body.appendChild(downloadLink);
     downloadLink.click();
     document.body.removeChild(downloadLink);
@@ -66,3 +71,8 @@ colorToggler.addEventListener('click', function () {
         resultField.style.color = '';
     }
 })
+
+function clearInput() {
+    inputField.value = '';
+    resultField.value = '';
+}
